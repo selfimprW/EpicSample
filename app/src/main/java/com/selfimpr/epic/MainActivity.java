cc2d9002fb1808b0ab553102349ca9580786c8c2
wangjiacheng <wangjiacheng@58ganji.com>
2019-12-03 23:53:05 +0800

    rxjava

--- app/src/main/java/com/selfimpr/epic/MainActivity.java
+++ app/src/main/java/com/selfimpr/epic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.selfimpr.epic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,6 +9,13 @@ import android.view.View;
 
 import de.robv.android.xposed.DexposedBridge;
 import de.robv.android.xposed.XC_MethodHook;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +38,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         DexposedBridge.findAndHookMethod(Thread.class, "run", new ThreadMethodHook());
+
+
+        Disposable disposable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) {
+                Log.e("main_rxjava", "Observable thread is : " + Thread.currentThread().getName());
+                e.onNext(1);
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) {
+                        Log.e("main_rxjava", "After observeOn(mainThread)，Current thread is " + Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(Schedulers.io())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) {
+                        Log.e("main_rxjava", "After observeOn(io)，Current thread is " + Thread.currentThread().getName());
+                    }
+                });
 
     }
 
